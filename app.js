8db245abb4829551ac63906182b1b492f0fb0982
sahrish-salman <sahrish.salman@gmail.com>
2022-11-03 16:08:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,226 @@
+// alert("Welcome JavaScript Programmer");
+
+// window.alert("JavaScript is your friend");
+
+// var hello="Hello JavaScript Programmer";
+// alert (hello);
+
+// var name ="Sahrish";
+// console.log(name);
+
+// var name ="Sahrish";
+// var name ="Salman";
+// console.log(name);
+
+// var nationality = "U.S.";
+// console.log(nationality);
+
+// var name = "Sahrish";  
+// var floogle = "Sahrish";
+// console.log(floogle);
+
+// var name = "Sahrish";  
+// var floogle = "Sahrish";
+// console.log(name);
+
+// var lessonAuthor = "Sahrish";
+// var guyWhoKeepsSayingHisOwnName = "Sahrish";
+// var x = "Sahrish";
+// console.log(lessonAuthor);
+
+// var lessonAuthor = "Sahrish";
+// var guyWhoKeepsSayingHisOwnName = "Sahrish";
+// var x = "Sahrish";
+// console.log(guyWhoKeepsSayingHisOwnName);
+
+// var lessonAuthor = "Sahrish";
+// var guyWhoKeepsSayingHisOwnName = "Sahrish";
+// var x = "Sahrish";
+// console.log(x);
+
+// var lastName = "Khan";
+// console.log(lastName);
+
+// var cityOfOrigin = "Karachi";
+// console.log(cityOfOrigin);
+
+// var aussieGreeting = "g'Day";
+// console.log(aussieGreeting);
+
+// var nickname = "Sahr";
+// console.log(nickname);
+
+// alert("Thanks for your input!");
+
+// var thanx = "Thanks for your input!"
+// alert(thanx);
+
+// var weight = 150;
+// var a = 25 ;
+// var total = weight + a;
+// console.log(total);
+
+// var weight = 150;
+// var a = 25 ;
+// var total = weight - a;
+// console.log(total);
+
+// var weight= 150;
+// var add = 25;
+// var weight = weight + add;
+// console.log(weight);
+
+// var weight= 150;
+// var sub = 25;
+// var weight = weight - sub;
+// console.log(weight);
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+// console.log(newNum);
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+// console.log(newNum);
+
+// var originalNum = 90;
+// var originalNum = originalNum + 10;
+// console.log(originalNum);
+
+// alert(144);
+
+// var caseQty = 144;
+// alert(caseQty);
+
+// var userAlert = 3;
+// var myVar = 2 ;
+// var rose = userAlert * myVar ;
+// console.log(rose);
+
+// var userName = 24;
+// var faveBreed = 3 ;
+// var userResponseTimeLimit = userName  / faveBreed ;
+// console.log(userResponseTimeLimit);
+
+// var popularNumber = 4;
+// var popularNumber = 2 + 2;
+// console.log(popularNumber);
+
+// alert(2 + 2);
+
+// alert(2+2+3+3-4-1);
+
+// var popularNumber = 12 - 24;
+// console.log(popularNumber);
+
+// var popularNumber = 3 * 12;
+// console.log(popularNumber);
+
+// var num = 10;
+// var popularNumber = num + 200;
+// console.log(popularNumber);
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// console.log(popularNumber);
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num - anotherNum;
+// console.log(popularNumber);
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num * anotherNum;
+// console.log(popularNumber);
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num / anotherNum;
+// console.log(popularNumber);
+
+// var whatsLeftOver = 10 % 3;
+// console.log(whatsLeftOver);
+
+// var whatsLeftOver = 9 % 3;
+// console.log(whatsLeftOver);
+
+// var num = 8;
+// num++;
+// console.log(num);
+
+// var num = 8;
+// ++num;
+// console.log(num);
+
+// var num = 8;
+// num--;
+// console.log(num);
+
+// var num = 8;
+// --num;
+// console.log(num);
+
+// var a = 10;
+// var b = ++a;
+// console.log(a);
+
+// var a = 10;
+// var b = ++a;
+// console.log(b);
+
+// var a = 10;
+// var b = 20;
+// var c = a++ - --b - a + ++a - b +a + --a + ++b - b;
+// console.log(c);
+
+// var num = 10;
+// var newNum = num++;
+// console.log(newNum);
+
+// var num = 10;
+// var newNum = ++num;
+// console.log(newNum);
+
+// var num = 10;
+// var newNum = num--;
+// console.log(newNum);
+
+// var num = 10;
+// var newNum = --num;
+// console.log(newNum);
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+
+// var totalCost = 1 + (3 * 4);
+// console.log(totalCost);
+
+// var totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+
+// var resultOfComputation = ((2 * 4) * 4) + 2;
+// console.log(resultOfComputation);
+
+// var resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation);
+
+// var message = "Thanks, ";
+// var userName = "Sahrish";
+// var banger = "!";
+// alert(message + userName + banger);
+
+// var message = "Thanks, ";
+// var userName = "Sahrish Khan";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+// alert("2" + "2");
+
+// alert("2 plus 2 equals " + 2 + 2);
